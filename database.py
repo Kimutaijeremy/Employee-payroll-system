27de58b414f1965e1d58a489d25ad265a4276a9d
Kimutaijeremy <jrmkimutai@gmail.com>
2025-12-04 19:58:04 +0300

    Database setup

--- database.py
+++ database.py
@@ -1,6 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
+from config import Config
+from models import Base
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+class Database:
+    def __init__(self):
+        self.engine = create_engine(Config.DATABASE_URL, echo=False)
+        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+    
+    def init_db(self):
+        """Initialize database and create tables"""
+        Base.metadata.create_all(bind=self.engine)
+        print("Database initialized successfully!")
+    
+    def get_session(self):
+        """Get database session"""
+        return self.SessionLocal()
+    
+    def close_session(self, session):
+        """Close database session"""
+        session.close()
+
+# Singleton database instance
+db = Database()
